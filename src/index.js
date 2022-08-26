1ec162dc0d7a9ae36eb0389bb90984e3ff0c0dbe
Akash Chaubey <akashakki9520@gmail.com>
2022-08-26 19:38:46 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<h1>Akash Chaubey</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Akash Chaubey neha chaubey</h1>, document.getElementById('root'));
 
 
