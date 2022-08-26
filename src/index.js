c4061484c9a0dcd7722f775af45252a9edd1ab12
akash <akash@krishaweb.net>
2022-08-27 01:03:31 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.render(<h1>Akash Chaubey</h1>, document.getElementById('root'));
