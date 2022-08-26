f74a42af85116103364e9170ec27af7de00e8196
akash <akash@krishaweb.net>
2022-08-27 01:04:41 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Akash Chaubey</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Akash Chaubey fsfsf
+  sdfsdf
+  sdf
+  sdfsdfsd
+  <fieldset>f
+    sdf
+    sdf
+    s
+    df
+    sd
+  </fieldset>
+</h1>, document.getElementById('root'));
