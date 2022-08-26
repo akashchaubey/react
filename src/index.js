61315adb94ba0e8b60fb604275d1532940449be2
akash <akash@krishaweb.net>
2022-08-27 01:41:43 +0530

    this change by vs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Akash Chaubey Neha Chaubey new changes</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Akash Chaubey Neha Chaubey new changes this change by vs</h1>, document.getElementById('root'));
