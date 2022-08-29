05a1cc21d9a74b4b306a7e5f7fb0ea820e32d2fc
Akash <akashchaubey.sky@gmail.com>
2022-08-29 18:47:21 +0000

    Update note.txt and index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Akash Chaubey Neha Chaubey new changes this change by vs</h1>, document.getElementById('root'));
+let jsExpressions = "JS Expressions";
+
+ReactDOM.render(
+   //React.Fragment or use <> </>
+    <> 
+    
+        <h1>Top 5 netflix series </h1>
+        <p>This is top 5 netflix series in world</p>
+        <ol>
+            <li>Daredevil</li>
+            <li>Money Heist</li>
+            <li>Narcos</li>
+            <li>Top Boy</li>
+            <li>Stranger Things</li>
+        </ol>
+    </>,
+document.getElementById('root'));
