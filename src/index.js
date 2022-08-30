eeb3eb13bac7ba360b925a7e62d2f6f8b42affd2
Akash <akashchaubey.sky@gmail.com>
2022-08-30 17:27:49 +0000

    Update note.txt and index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let jsExpressions = "JS Expressions";
+const name = "JS Expressions";
 
 ReactDOM.render(
    //React.Fragment or use <> </>
     <> 
-    
+        <h1>how to use { name } in this project</h1>
+        <h1>how to use { 3 * 3 } in this project</h1>
+        <h1>Math random number = { Math.random() }</h1>
         <h1>Top 5 netflix series </h1>
         <p>This is top 5 netflix series in world</p>
         <ol>
