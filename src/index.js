11044dc3cc7fb887ed75f335c7a034dd4bd4ffe5
Akash <akashchaubey.sky@gmail.com>
2022-08-31 17:50:01 +0000

    Update note.txt and index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const name = "JS Expressions";
+let fname = "akash";
+let lname = "chaubey";
+let currDate = new Date().toLocaleDateString();
+let currTime = new Date().toLocaleTimeString();
 
 ReactDOM.render(
    //React.Fragment or use <> </>
@@ -9,6 +13,10 @@ ReactDOM.render(
         <h1>how to use { name } in this project</h1>
         <h1>how to use { 3 * 3 } in this project</h1>
         <h1>Math random number = { Math.random() }</h1>
+        <h2>Use of template literals Option 1 = {fname} {lname}</h2>
+        <h2>Use of template literals Option 2 = {fname + lname}</h2>
+        <h2>Use of template literals Option 3 = {fname+" "+lname}</h2>
+        <h2>{`Use of template literals Option 4 = ${fname} ${lname}`}</h2>
         <h1>Top 5 netflix series </h1>
         <p>This is top 5 netflix series in world</p>
         <ol>
@@ -18,5 +26,10 @@ ReactDOM.render(
             <li>Top Boy</li>
             <li>Stranger Things</li>
         </ol>
+        
+        <h1>Date Chalange</h1>
+        <p>Current Date = {currDate}</p>
+        <p>Current Time = {currTime}</p>
+        
     </>,
 document.getElementById('root'));
